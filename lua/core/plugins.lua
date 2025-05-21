74d7a67ad7b5a6cecee4420af444397c1d9f4873
saumyadip biswas <saumyadip11@gmail.com>
2025-05-22 00:03:21 +0530

    New plugins added for color detection in files and for project management

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,13 +1,12 @@
-return {
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons"
-    },
-    config = function()
-      require("nvim-tree").setup({})
-    end
+return { {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons"
   },
+  config = function()
+    require("nvim-tree").setup({})
+  end
+},
   {
     "catppuccin/nvim",
     name = "catppuccin",
@@ -100,6 +99,43 @@ return {
           timeout_ms = 500,
           lsp_fallback = true,
         },
+      })
+    end
+  },
+  -- Prject manager
+  {
+    "coffebar/neovim-project",
+    opts = {
+      projects = { -- define project roots
+        "~/projects/*",
+        "~/.config/*",
+      },
+      picker = {
+        type = "telescope", -- or "fzf-lua"
+      }
+    },
+    init = function()
+      -- enable saving the state of plugins in the session
+      vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+    end,
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      -- optional picker
+      { "nvim-telescope/telescope.nvim", tag = "0.1.4" },
+      -- optional picker
+      { "ibhagwan/fzf-lua" },
+      { "Shatur/neovim-session-manager" },
+    },
+    lazy = false,
+    priority = 100,
+  },
+  {
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
+    opts = {},
+    config = function()
+      require("colorizer").setup({
+        sass = { enable = true, parsers = { "css" } },
       })
     end
   },
